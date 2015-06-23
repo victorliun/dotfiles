865c9aa2fe911991644a932405adf64cbc14bce4
Victor Liu <victor.ns.liu@outlook.com>
2015-06-23 09:53:31 +0100

    add .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,31 @@
+;;;Project structure layout
+(add-to-list 'load-path "~/.emacs.d/vendor/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+
+;; lua2 mode
+(require 'lua2-mode)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua2-mode))
+
+;; Yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; Auto complete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Dockerfile mode
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;; flake8 check
+(require 'flake8)
+
+;disable backup
+(setq backup-inhibited t)
+;disable auto save
+(setq auto-save-default nil)
+
+;;; speed up movement.
